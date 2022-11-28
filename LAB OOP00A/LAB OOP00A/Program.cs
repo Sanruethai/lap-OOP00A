ffe5754d6c9b69eccacf37410f1b6db5fed68320
Sanruethai <sanruethai.s@kkumail.com>
2022-11-28 16:08:08 +0700

    Uplode

---
+++ LAB OOP00A/LAB OOP00A/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sanruethai Saybut");
+Console.WriteLine("653450104-1");
+Console.WriteLine("sanruethai.s@kkumail.com");
+
